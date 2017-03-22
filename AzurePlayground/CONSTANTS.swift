65ba8cdfc5be53188e5e0c2fc16d297492de9a46
Eric Risco <eric.risco@andornet.ad>
2017-03-22 09:22:59 +0100

    Constants

---
+++ AzurePlayground/CONSTANTS.swift
@@ -0,0 +1,16 @@
+//
+//  CONSTANTS.swift
+//  AzurePlayground
+//
+//  Created by Eric Risco de la Torre on 22/03/2017.
+//  Copyright © 2017 ERISCO. All rights reserved.
+//
+
+import Foundation
+
+struct CONSTANTS {
+    
+    static let ACCOUNT_NAME = "YOUR AZURE ACCOUNT NAME"
+    static let ACCOUNT_KEY = "YOUR AZURE ACCOUNT KEYgit"
+    
+}
